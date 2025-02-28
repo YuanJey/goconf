f5b39919a015a5921e73ee42d640c28b8ab875ed
yuanjey <2218773049@qq.com>
2025-02-28 14:14:45 +0800

    1

--- test/main.go
+++ test/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/YuanJey/goconf/pkg/config"
+	"os"
 )
 
 func main() {
+	//os.Setenv("test2", "{\"a\":\"12\"}")
+	err := json.Unmarshal([]byte("{\"a\":\"12\"}"), &config.Config.Test2)
+	if err != nil {
+	}
 	fmt.Println(config.Config)
 	fmt.Println(config.Config.Test.GoVERSION)
+	fmt.Println(config.Config.Test2)
+
+	fmt.Println(os.Getenv("test2"))
 }
